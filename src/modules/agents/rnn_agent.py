4bb056ef2217d2096d32e5d1b51f761770c86de9
ignaciocarlucho <ignacio.carlucho@gmail.com>
2022-10-15 18:54:17 +0100

    if no rnn is used None can be passed as hidden state

--- src/modules/agents/rnn_agent.py
+++ src/modules/agents/rnn_agent.py
@@ -20,8 +20,9 @@ class RNNAgent(nn.Module):
 
     def forward(self, inputs, hidden_state):
         x = F.relu(self.fc1(inputs))
-        h_in = hidden_state.reshape(-1, self.args.hidden_dim)
+        
         if self.args.use_rnn:
+            h_in = hidden_state.reshape(-1, self.args.hidden_dim)
             h = self.rnn(x, h_in)
         else:
             h = F.relu(self.rnn(x))

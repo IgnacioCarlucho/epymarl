5e8f9ecaa095ef99c798431597aaec343e6d17b8
ignaciocarlucho <ignacio.carlucho@gmail.com>
2022-10-15 19:41:44 +0100

    changes for running maddpg

--- lbf_run.sh
+++ lbf_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CUDA_VISIBLE_DEVICES="0"
-python3 src/main.py --config=qmix --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2"  seed=1234
+# python3 src/main.py --config=qmix --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2"  seed=1234
 
 # python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=1234
 
@@ -16,3 +16,13 @@ python3 src/main.py --config=qmix --env-config=gymmb with env_args.time_limit=50
 # python3 src/main.py --config=mappo --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=55
 
 # python3 src/main.py --config=iql --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=55
+
+
+python3 src/main.py --config=maddpg --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=1234
+
+# python3 src/main.py --config=maddpg --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=55  
+
+# python3 src/main.py --config=maddpg --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=753
+
+# python3 src/main.py --config=maa2c --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=1234
+

7bfaadb01676d779cb08b116f8804f8ebba52064
ignaciocarlucho <ignacio.carlucho@gmail.com>
2022-10-17 15:53:30 +0100

    adding config for running corridor 2

--- corridor_run.sh
+++ corridor_run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 python3 src/main.py --config=qmix --env-config=corridor2d with env_args.time_limit=60 env_args.key="coopreaching:MARL-CooperativeReaching-5-40-v0"  seed=1234
+#python3 src/main.py --config=qmix --env-config=corridor2d with env_args.time_limit=60 env_args.key="coopreaching:MARL-CooperativeReaching-5-40-v0"  seed=55
+#python3 src/main.py --config=qmix --env-config=corridor2d with env_args.time_limit=60 env_args.key="coopreaching:MARL-CooperativeReaching-5-40-v0"  seed=753
 
 # python3 src/main.py --config=ippo --env-config=corridor2d with env_args.time_limit=50 env_args.key="coopreaching:MARL-CooperativeReaching-5-40-v0" seed=1234
+# python3 src/main.py --config=ippo --env-config=corridor2d with env_args.time_limit=50 env_args.key="coopreaching:MARL-CooperativeReaching-5-40-v0" seed=55
+# python3 src/main.py --config=ippo --env-config=corridor2d with env_args.time_limit=50 env_args.key="coopreaching:MARL-CooperativeReaching-5-40-v0" seed=753
 
 # python3 src/main.py --config=mappo --env-config=corridor2d with env_args.time_limit=50 env_args.key="coopreaching:MARL-CooperativeReaching-5-40-v0" seed=1234
+# python3 src/main.py --config=mappo --env-config=corridor2d with env_args.time_limit=50 env_args.key="coopreaching:MARL-CooperativeReaching-5-40-v0" seed=55
+# python3 src/main.py --config=mappo --env-config=corridor2d with env_args.time_limit=50 env_args.key="coopreaching:MARL-CooperativeReaching-5-40-v0" seed=753

6b837d80a977cc8836afe29aabbb69dd9f65ab31
ignaciocarlucho <ignacio.carlucho@gmail.com>
2022-11-14 12:05:20 +0000

    re doing the LBF runs for 3f

--- lbf_run.sh
+++ lbf_run.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 export CUDA_VISIBLE_DEVICES="0"
-# python3 src/main.py --config=qmix --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2"  seed=1234
-
-# python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=1234
-
-# python3 src/main.py --config=mappo --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=1234
-
-# python3 src/main.py --config=iql --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=1234
-
-# python3 src/main.py --config=qmix --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2"  seed=55
-
-# python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=55
-
-# python3 src/main.py --config=mappo --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=55
-
-# python3 src/main.py --config=iql --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=55
+p# ython3 src/main.py --config=qmix --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2"  seed=1234 descriptor=_a
+# python3 src/main.py --config=qmix --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2"  seed=5677
+# python3 src/main.py --config=qmix --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2"  seed=12340
 
 
-python3 src/main.py --config=maddpg --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=1234
+# python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2" seed=1234
+# python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2" seed=55
 
-# python3 src/main.py --config=maddpg --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=55  
 
-# python3 src/main.py --config=maddpg --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=753
 
-# python3 src/main.py --config=maa2c --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-4f-coop-v2" seed=1234
+# python3 src/main.py --config=mappo --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2" seed=1234
+# python3 src/main.py --config=mappo --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2" seed=55
+
+# python3 src/main.py --config=iql --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2" seed=55
+# python3 src/main.py --config=iql --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2" seed=1234
+
+# python3 src/main.py --config=maddpg --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2" seed=1234
+# python3 src/main.py --config=maddpg --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2" seed=55  
+# python3 src/main.py --config=maddpg --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2" seed=753
+
+
+# python3 src/main.py --config=maa2c --env-config=gymmb with env_args.time_limit=50 env_args.key="lbforaging:Foraging-6x6-2p-3f-coop-v2" seed=1234
 

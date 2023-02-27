010e8d05bdde5b62502c1bdd8c42671ce84fa085
ignaciocarlucho <ignacio.carlucho@gmail.com>
2023-02-27 14:24:01 +0000

    run update

--- grid_run.sh
+++ grid_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=50 env_args.key="grid_generalization:grid-generalization-2p-1w-v0" seed=4321
-python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=50 env_args.key="grid_generalization:grid-generalization-2p-2w-v0" seed=4321
-python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=50 env_args.key="grid_generalization:grid-generalization-2p-3w-v0" seed=4321
-python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=50 env_args.key="grid_generalization:grid-generalization-2p-4w-v0" seed=4321
-python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=50 env_args.key="grid_generalization:grid-generalization-2p-5w-v0" seed=4321
+python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=200 env_args.key="grid_generalization:grid-generalization-2p-1w-v0" seed=4321
+python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=200 env_args.key="grid_generalization:grid-generalization-2p-2w-v0" seed=4321
+python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=200 env_args.key="grid_generalization:grid-generalization-2p-3w-v0" seed=4321
+python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=200 env_args.key="grid_generalization:grid-generalization-2p-4w-v0" seed=4321
+python3 src/main.py --config=ippo --env-config=gymmb with env_args.time_limit=200 env_args.key="grid_generalization:grid-generalization-2p-5w-v0" seed=4321
